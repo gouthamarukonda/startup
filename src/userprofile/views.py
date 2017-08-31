554e9b7359167e87d02b285f7d88957029019b93
Avinash <avinashdec12@gmail.com>
2017-09-01 02:59:31 +0530

    login written and debugged approve and disapprove

--- src/userprofile/views.py
+++ src/userprofile/views.py
@@ -23,7 +23,8 @@ def user_approve(request):
 				if User.objects.filter(username = request.POST.get("username")).exists():
 					user = User.objects.get(username = request.POST.get("username"))
 					user.is_active = True
-					user.userprofile__status = STATUS_APPROVED
+					user.userprofile.status = STATUS_APPROVED
+					user.userprofile.save()
 					user.save()
 					return JsonResponse({"status": True, "msg": "User Approved Sucessfully"})
 				else:
@@ -45,7 +46,8 @@ def user_disapprove(request):
 				if User.objects.filter(username = request.POST.get("username")).exists():
 					user = User.objects.get(username = request.POST.get("username"))
 					user.is_active = False
-					user.userprofile__status = STATUS_UNAPPROVED
+					user.userprofile.status = STATUS_UNAPPROVED
+					user.userprofile.save()
 					user.save()
 					return JsonResponse({"status": True, "msg": "User DisApproved Sucessfully"})
 				else:
@@ -58,21 +60,28 @@ def user_disapprove(request):
 def user_login(request):
 	if request.method == 'POST':
 		try:
-			user = authenticate(username = request.POST.get("username"), password = request.POST.get("password"))
-			if user is not None:
-				if user.is_superuser:
-					login(request, user)
-					return JsonResponse({"status": True , "msg": "admin login"})
-				if user.userprofile.status != STATUS_APPROVED:
-					login(request, user)
-					if user.userprofile.role == ROLE_STUDENT:
-						return JsonResponse({"status": True , "msg": "student login"})
-					if user.userprofile.role == ROLE_TEACHER:
-						return JsonResponse({"status": True , "msg": "teacher login"})
-				else:
-					return JsonResponse({"status": False, "msg": "Wait for your approval"})
+			if not request.POST.get("username"):
+				return JsonResponse({"status": False, "msg": "User ID shouldn't be empty"})
 			else:
-				return JsonResponse({"status": False, "msg": "Credentials don't match"})
+				if User.objects.filter(username = request.POST.get("username")).exists():
+					user_object = User.objects.get(username = request.POST.get("username"))
+					if not(user_object.is_superuser):
+						if user_object.userprofile.status != STATUS_APPROVED :
+							return JsonResponse({"status": False, "msg": "Wait for your approval"})
+					user = authenticate(username = request.POST.get("username"), password = request.POST.get("password"))
+					if user is not None:
+						login(request, user)
+						if user.is_superuser:
+							return JsonResponse({"status": True , "msg": "admin login"})
+						if user.userprofile.role == ROLE_STUDENT:
+							return JsonResponse({"status": True , "msg": "student login"})
+						if user.userprofile.role == ROLE_TEACHER:
+							return JsonResponse({"status": True , "msg": "teacher login"})
+					else:
+						
+						return JsonResponse({"status": False, "msg": "Credentials don't match"})
+				else:
+					return JsonResponse({"status": False, "msg": "User Doesn't exist"})
 		except:
 			return JsonResponse({"status": False, "msg": "Internal Server Error"})
 
