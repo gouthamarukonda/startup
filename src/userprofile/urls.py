5aa2f4829808c0d4b8cdf58b6f063ea8f4f75b8d
gouthamarukonda <gouthamarukonda@gmail.com>
2017-09-17 14:34:01 +0530

    Added login frontend and all static files

--- src/userprofile/urls.py
+++ src/userprofile/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
 	url(r'^approve/$', views.user_approve),
 	url(r'^disapprove/$', views.user_disapprove),
 	url(r'^login/$', views.user_login),
+	url(r'^home/$', views.get_user_home_page),
+
 ]
